68e699af716cc781c0c8cf6a60b6a00155435194
jyeric <jyeric1@126.com>
2025-09-26 12:54:27 +0800

    fix(oi-wiki-export): mem limit too big

--- oi-wiki-export/increase-mem-limit.py
+++ oi-wiki-export/increase-mem-limit.py
@@ -6,7 +6,7 @@ import subprocess
 config_file = subprocess.check_output(["kpsewhich", "texmf.cnf"]).decode().strip()
 
 memory_setting = "main_memory"
-new_value = "50000000"
+new_value = "16000000"
 
 output_lines = []
 with open(config_file, "r") as f:
